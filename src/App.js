77212919ef26fc35c52d1c722b18f011352f6b27
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-01-20 15:24:50 +0530

    implement client side and set blockchain boiler plate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import bell from "./assets/images/bell.png";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: null,
+      contract: null,
+      recordHash: "",
+      account: ""
+    };
+  }
+
+  captureFile = event => {
+    event.preventDefault();
+    console.log("file captured");
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log("buffer", this.state.buffer);
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <div>SURAKSHIT</div>
+          <div>
+            <img src={bell} height="40px" width="40px" alt="bell" />
+            <div className="notification-circle">
+              <p className="notification-number">1</p>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="control-panel">CONTROL PANEL</div>
+          <div className="form-container">
+            <div>
+              Upload Report
+              <form>
+                <label>
+                  Place of Crime
+                  <input type="text" placeholder="Enter Place of Crime" />
+                </label>
+                <label>
+                  Upload Record
+                  <input
+                    type="file"
+                    placeholder="Upload File"
+                    onChange={this.captureFile}
+                  />
+                </label>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </div>
+          <div className="report-container">
+            <div className="report-card">
+              <div>Document : </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
